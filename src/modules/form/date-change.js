8b6d0006f9f45d696676c5c1f7da4ce08eb72181
Lucas Vitor Lima Viana <lucasvitorlv@gmail.com>
2025-05-13 21:33:25 -0300

    finalizado a manipulação do form

---
+++ src/modules/form/date-change.js
@@ -0,0 +1,11 @@
+import { hoursLoad } from "../form/hours-load.js"
+
+//Seleciona o input de data.
+const selectedDate = document.getElementById("date")
+
+export function schedulesDay() {
+  const date = selectedDate.value
+
+  //Renderiza as horas disponíveis.
+  hoursLoad({ date })
+}
