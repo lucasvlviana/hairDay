7e02e42412a3af09466afb04005aad2c9d27ac7d
Lucas Viana <70477354+lucasvlviana@users.noreply.github.com>
2025-05-16 16:54:32 -0300

    removendo agendamento

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -7,6 +7,14 @@ periods.forEach((periods) => {
     if (event.target.classList.contains("cancel-icon")) {
       const item = event.target.closest("li")
       const { id } = item.dataset
+
+      if (id) {
+        const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?")
+
+        if (isConfirm) {
+          console.log("REMOVER!")
+        }
+      }
     }
   })
 })
